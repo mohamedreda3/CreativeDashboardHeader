5376a91304f5f8f9c0aab8f4a2265d7d3f86e2fa
mohamedreda3 <73368901+mohamedreda3@users.noreply.github.com>
2022-09-25 14:28:36 +0200

    commit

---
+++ main.js
@@ -0,0 +1,31 @@
+// var li = document.querySelectorAll('ul.toggle li');
+// li.forEach(item => {
+//     item.onclick = () => {
+//         li.forEach(lis => lis.classList.remove("active"));
+//         item.classList.add('active');
+//     }
+// });
+
+
+let openDrop = document.querySelectorAll('.open-drop');
+openDrop.forEach(ele => {
+    ele.onclick = (e) => {
+        document.querySelectorAll('.drop').forEach(item => item.classList.remove('active'));
+        openDrop.forEach(lis => lis.classList.remove("active"));
+        ele.classList.add('active');
+        if (e.currentTarget.nextElementSibling != null) {
+            e.currentTarget.nextElementSibling.classList.add('active');
+        }
+    }
+});
+
+let dropLi = document.querySelectorAll('ul.dropmenu li');
+
+dropLi.forEach(item => {
+    item.addEventListener('click', (e) => {
+        dropLi.forEach(dropLis => dropLis.classList.remove("active"));
+        console.log(e.currentTarget);
+        e.currentTarget.classList.add('active');
+    }, { capture: false });
+});
+
